8e226fd90660e639187f832f3c90df44f8206bb0
Rohith K Binu <135629947+Rohith-Keenthananickal@users.noreply.github.com>
2026-01-29 16:58:31 +0530

    url

--- environment.tsx
+++ environment.tsx
@@ -1,6 +1,6 @@
 export const environment = {
     apiUrl: 'https://api.accessrooms.com/',
-    userId: '66af62337fdec4991cb3015a',
-    // userId: '69099bd245a573d9b9cc1f9c',
+    // userId: '66af62337fdec4991cb3015a',
+    userId: '69099bd245a573d9b9cc1f9c',
     imageBaseUrl: 'https://accessrooms.s3.ap-south-1.amazonaws.com/'
 }
