3fafff21e4f5d59bddb9a3902f63684a0b6b8cc2
Rohith K Binu <135629947+Rohith-Keenthananickal@users.noreply.github.com>
2026-04-06 00:17:57 +0530

    Update hotel branding and versioning
    - Change application title, description, and Open Graph/Twitter metadata from "Ayur Hills Retreat" to "Wayanad Royal Stream".
    - Update userId in environment.tsx.
    - Increment version in package.json to 0.0.4.

--- environment.tsx
+++ environment.tsx
@@ -1,6 +1,6 @@
 export const environment = {
     apiUrl: 'https://api.accessrooms.com/',
     // userId: '66af62337fdec4991cb3015a',
-    userId: '69a9494b82b37f0e97fa6c9d',
+    userId: '6886180b66d42c5ed85046d6',
     imageBaseUrl: 'https://accessrooms.s3.ap-south-1.amazonaws.com/'
 }
