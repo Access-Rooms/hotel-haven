5b2c8ed026d0c03eddc34e5d1b64ef58ea93f961
Rohith K Binu <135629947+Rohith-Keenthananickal@users.noreply.github.com>
2026-06-22 01:21:07 +0530

    Update environment configuration and increment version in package.json to 0.2.2
    - Change imageBaseUrl from S3 to Google Cloud Storage for improved asset management.
    - Increment version in package.json to reflect recent changes.

--- environment.tsx
+++ environment.tsx
@@ -2,5 +2,5 @@ export const environment = {
     apiUrl: 'https://api.accessrooms.com/',
     // userId: '66af62337fdec4991cb3015a',
     userId: '69099bd245a573d9b9cc1f9c',
-    imageBaseUrl: 'https://accessrooms.s3.ap-south-1.amazonaws.com/'
+    imageBaseUrl: 'https://storage.googleapis.com/accessrooms-storage/'
 }
