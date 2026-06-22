192c7fab8ac5808414e97be88d7e35fbd386309d
Rohith K Binu <135629947+Rohith-Keenthananickal@users.noreply.github.com>
2026-06-22 16:55:15 +0530

    Update environment configuration and increment version
    - Change imageBaseUrl in environment.tsx to use Google Cloud Storage.
    - Increment version in package.json to 0.0.5.

--- environment.tsx
+++ environment.tsx
@@ -2,5 +2,5 @@ export const environment = {
     apiUrl: 'https://api.accessrooms.com/',
     // userId: '66af62337fdec4991cb3015a',
     userId: '6886180b66d42c5ed85046d6',
-    imageBaseUrl: 'https://accessrooms.s3.ap-south-1.amazonaws.com/'
+    imageBaseUrl: 'https://storage.googleapis.com/accessrooms-storage/'
 }
