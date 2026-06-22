de0061854d9db806c3310648c42c127b934e8664
Rohith K Binu <135629947+Rohith-Keenthananickal@users.noreply.github.com>
2026-06-22 17:04:52 +0530

    Add new API URL for version 2 and update room service to utilize it
    - Introduce apiUrl_V2 in environment.tsx for accessing the new vendor API.
    - Update getRoomAvailability method in RoomService to use the new apiUrl_V2 for fetching room availability data.

--- environment.tsx
+++ environment.tsx
@@ -1,5 +1,7 @@
 export const environment = {
     apiUrl: 'https://api.accessrooms.com/',
+    apiUrl_V2: 'https://vendor.accessrooms.com/api/',
+
     // userId: '66af62337fdec4991cb3015a',
     userId: '69a9494b82b37f0e97fa6c9d',
     imageBaseUrl: 'https://storage.googleapis.com/accessrooms-storage/'
